7667ab15a7f3cbe7fbed11f54f6b384e858e2b52
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-01-13 10:24:51 -0800

    Update spline_helper_tests.py

--- common_needs/spline_helper_tests.py
+++ common_needs/spline_helper_tests.py
@@ -16,13 +16,67 @@ path.insert(0, str(infrastructure_folder.joinpath("common_needs")))
 
 # Internal modules
 from spline_helper import *
+from tkinter_helper import askSaveFilename
 
 # External modules
+import matplotlib.pyplot as plt
+from numpy import random
 
 
 ########################################################
 ### Define unit tests for the various spline classes ###
 ########################################################
+# Set the needed parameters
+seed = 2
+n_points = 7
+x_min = -10
+x_max = 20
+y_min = -3
+y_max = 8
+
+# Set the random seed
+random.seed(seed = seed)
+
+# Randomly generate the x-values and y-values
+# Initialize the lists
+x_values = []
+y_values = []
+# Generate the random values
+for _ in range(n_points):
+	x_values.append(x_min + (x_max - x_min) * random.rand())
+	y_values.append(y_min + (y_max - y_min) * random.rand())
+# Sort the x-values to be in increasing order
+x_values.sort(reverse = False)
+
+# Compute the lower and upper x-values to use
+x_lower = 1.1 * min(x_values) - 0.1 * max(x_values)
+x_upper = 1.1 * max(x_values) - 0.1 * min(x_values)
+
+# Create linear and natural cubic splines using these x-values and y-values
+linear_spline = LinearSpline(x_values = x_values, y_values = y_values)
+cubic_spline = NaturalCubicSpline(x_values = x_values, y_values = y_values)
+
+# Generate the plot information for each spline
+linear_plot_results = linear_spline.getPlotInfo(x_lower = x_lower, x_upper = x_upper)
+cubic_plot_results = cubic_spline.getPlotInfo(x_lower = x_lower, x_upper = x_upper)
+
+# Create a plot overlaying the two splines
+# Create the figure
+plt.figure(figsize = (10, 8))
+# Add the needed traces
+plt.plot(linear_plot_results["x_values_interpolated"], linear_plot_results["y_values_interpolated"], "b-", zorder = 0, label = "Linear")
+plt.plot(cubic_plot_results["x_values_interpolated"], cubic_plot_results["y_values_interpolated"], "g-", zorder = 5, label = "Cubic")
+plt.scatter(x_values, y_values, None, "k", zorder = 10)
+# Format the figure
+plt.title("Overlay Of Linear And Natural Cubic Splines")
+plt.xlabel("x-value")
+plt.ylabel("y-value")
+plt.legend()
+# Save the figure
+image_path = askSaveFilename(allowed_extensions = ["png"])
+assert image_path is not None, "Unable to save matplotlib figure because cancel button was clicked"
+plt.savefig(image_path)
+
 '''
 x_values = [0, 3, 5, 9, 11]
 y_values = [1, -2, 0, 0, 4]
