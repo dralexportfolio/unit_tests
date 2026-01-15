6e1153876951827bfd9ea6d4324f94bebf57fae0
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-01-15 11:09:15 -0800

    Update Polygon_tests.py

--- board_games/Polygon_tests.py
+++ board_games/Polygon_tests.py
@@ -28,7 +28,7 @@ bevel_attitude = 25
 bevel_size = 0.1
 sun_angle = 120
 sun_attitude = 35
-dpi = 900
+dpi = 600
 tint_shade = RGB((255, 0, 255))
 
 # Preprocess the bevel information for each polygon
@@ -39,6 +39,8 @@ TRIANGLE_1x1_SE.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size 
 TRIANGLE_1x1_SW.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
 HEXAGON_2x3.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
 HEXAGON_3x2.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+HEXAGON_REGULAR_TALL.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+HEXAGON_REGULAR_WIDE.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
 
 # Preprocess the sun information for each polygon
 SQUARE_1x1.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
@@ -48,6 +50,8 @@ TRIANGLE_1x1_SE.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_atti
 TRIANGLE_1x1_SW.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
 HEXAGON_2x3.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
 HEXAGON_3x2.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
+HEXAGON_REGULAR_TALL.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
+HEXAGON_REGULAR_WIDE.preprocessSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
 
 # Render each polygon
 SQUARE_1x1.render(dpi = dpi, tint_shade = tint_shade).show()
@@ -57,3 +61,5 @@ TRIANGLE_1x1_SE.render(dpi = dpi, tint_shade = tint_shade).show()
 TRIANGLE_1x1_SW.render(dpi = dpi, tint_shade = tint_shade).show()
 HEXAGON_2x3.render(dpi = dpi, tint_shade = tint_shade).show()
 HEXAGON_3x2.render(dpi = dpi, tint_shade = tint_shade).show()
+HEXAGON_REGULAR_TALL.render(dpi = dpi, tint_shade = tint_shade).show()
+HEXAGON_REGULAR_WIDE.render(dpi = dpi, tint_shade = tint_shade).show()
