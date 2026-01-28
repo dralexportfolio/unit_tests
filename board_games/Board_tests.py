8b505cff21c9611e4db944fd3de0a702ab65ff33
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-01-27 20:19:31 -0800

    Update Board_tests.py

--- board_games/Board_tests.py
+++ board_games/Board_tests.py
@@ -19,6 +19,8 @@ path.insert(0, str(infrastructure_folder.joinpath("common_needs")))
 from Board import *
 from color_helper import RGB
 from Polygon import *
+from tkinter_helper import askSaveFilename
+
 
 #########################################################
 ### Define unit tests for the various Polygon objects ###
@@ -30,8 +32,9 @@ bevel_attitude = 25
 bevel_size = 0.1
 sun_angle = 120
 sun_attitude = 35
-dpi = 600
-tint_shade = RGB((255, 255, 255))
+dpi = 300
+tint_shade_1 = RGB((200, 200, 200))
+tint_shade_2 = RGB((200, 127, 100))
 
 # Set the number of polygons and create the associated list
 n_polygons = n_rows * n_cols
@@ -68,7 +71,12 @@ board.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_si
 board.preprocessAllSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
 
 # Set the tint shade for all the polygons on the board
-board.setTintShade(tint_shade = tint_shade)
+for row_index in range(n_rows):
+	for col_index in range(n_cols):
+		tint_shade = tint_shade_1 if (row_index + col_index) % 2 == 0 else tint_shade_2
+		board.setTintShade(tint_shade = tint_shade, polygon_index = col_index + n_cols * row_index)
 
-# Render the board at the needed DPI and with the needed tint
-board.render(dpi = dpi).show()
+# Render the board with the needed settings and save it to the given location
+filename_path = askSaveFilename(allowed_extensions = ["png"])
+assert filename_path is not None, "Unable to proceed with saving render because no filename was selected"
+board.render(dpi = dpi).save(filename_path)
