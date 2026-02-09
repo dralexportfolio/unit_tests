11a98b708cf1819122443bec27ac422c5dd8b761
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-02-08 17:35:56 -0800

    Change board test result to animation, fixed flicker by changing from gif to mp4

--- board_games/Board_tests.py
+++ board_games/Board_tests.py
@@ -21,18 +21,30 @@ from color_helper import RGB
 from Polygon import *
 from tkinter_helper import askSaveFilename
 
+# External modules
+from imageio.v2 import get_writer, imread
+from os import remove
+from math import pi, sin
+from tqdm import tqdm
 
 #########################################################
 ### Define unit tests for the various Polygon objects ###
 #########################################################
-# Set the needed parameters
+# Set all of the needed parameters
+# Board size
 n_rows = 8
 n_cols = 8
+# Bevel settings
 bevel_attitude = 25
 bevel_size = 0.1
-sun_angle = 120
-sun_attitude = 35
+# Initial sun settings
+initial_sun_angle = 120
+initial_sun_attitude = 35
+# Animation settings
+n_frames = 100
+frame_duration = 50
 dpi = 300
+# Tint settings
 tint_shade_1 = RGB((200, 200, 200))
 tint_shade_2 = RGB((200, 127, 100))
 
@@ -66,17 +78,41 @@ board = Board(n_polygons = n_polygons,
 			  x_shift_per_polygon = x_shift_per_polygon,
 			  y_shift_per_polygon = y_shift_per_polygon)
 
-# Preprocess the needed bevel and sun information
-board.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
-board.preprocessAllSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
-
 # Set the tint shade for all the polygons on the board
 for row_index in range(n_rows):
 	for col_index in range(n_cols):
 		tint_shade = tint_shade_1 if (row_index + col_index) % 2 == 0 else tint_shade_2
 		board.setTintShade(tint_shade = tint_shade, polygon_index = col_index + n_cols * row_index)
 
-# Render the board with the needed settings and save it to the given location
-filename_path = askSaveFilename(allowed_extensions = ["png"])
+# Preprocess the bevel information for all polygons on the board
+board.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+
+# Ask for a path to which an animation should be saved, raise error if not selected
+filename_path = askSaveFilename(allowed_extensions = ["mp4"])
 assert filename_path is not None, "Unable to proceed with saving render because no filename was selected"
-board.render(dpi = dpi).save(filename_path)
+
+# Generate a sequence of board renders and save them to a single animation file
+with get_writer(filename_path, fps = int(1000 / frame_duration)) as video_writer:
+	for frame_index in tqdm(range(n_frames)):
+		# Set the sun angle and attitude accordingly
+		sun_angle = (initial_sun_angle + 360 * frame_index / n_frames) % 360
+		sun_attitude = initial_sun_attitude + (65 - initial_sun_attitude) * sin(pi * frame_index / n_frames)**2
+
+		# Preprocess the needed sun information
+		board.preprocessAllSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
+
+		# Get the path for the current image file, append to the list, and render the current image
+		current_filename_path = filename_path.parent.joinpath(filename_path.name[:-4] + "_" + str(frame_index) + ".png")
+		current_image = board.render(dpi = dpi)
+
+		# Save the current image and delete the PIL object
+		current_image.save(current_filename_path)
+		current_image.close()
+
+		# Load the generated image using imageio, add it to the video file, then delete the loaded image object and associated file
+		# Add rendered image to the gif
+		loaded_image = imread(current_filename_path)
+		video_writer.append_data(loaded_image)
+		# Delete the original render
+		del loaded_image
+		remove(current_filename_path)
